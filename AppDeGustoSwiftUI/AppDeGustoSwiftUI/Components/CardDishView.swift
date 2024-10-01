18a7557efd75a1afc9fc4943d01f68b363209b88
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-09-30 20:06:29 -0500

    Diseño del detalle del negocio BusinessView

---
+++ AppDeGustoSwiftUI/AppDeGustoSwiftUI/Components/CardDishView.swift
@@ -0,0 +1,45 @@
+//
+//  CardDishView.swift
+//  AppDeGustoSwiftUI
+//
+//  Created by YeltsinMacPro13 on 30/09/24.
+//
+
+import SwiftUI
+
+struct CardDishView: View {
+    let name: String
+    let description:String
+    let image: Image
+    let price: String
+    let size: CGFloat
+    var body: some View {
+        VStack (alignment: .leading){
+            image
+                .resizable()
+                .frame(width: size, height: 200 * (size/210))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            Text(name)
+                .font(.title3)
+//                .fontWeight(.bold)
+            VStack(alignment: .leading,spacing: 2) {
+                Text(description)
+                    .lineSpacing(8)
+                    .opacity(0.4)
+                Text("S/ \(price)")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+//                    .padding(.leading)
+            }
+//            .frame(maxWidth: .infinity)
+        }
+        .frame(width: size)
+//        .padding()
+//        .background(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+}
+
+#Preview {
+    CardDishView(name:"Conchitas Parmesanas",description: "Descripcion de todos los platos para visualizarlo cuando se esta en el restaurante y el propio plato",image: Image("plato1"), price: "25.0", size: 180)
+}
--- AppDeGustoSwiftUI/AppDeGustoSwiftUI/Core/Home/BusinessView.swift
+++ AppDeGustoSwiftUI/AppDeGustoSwiftUI/Core/Home/BusinessView.swift
@@ -6,13 +6,67 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct BusinessView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Color("Bg")
+                    .ignoresSafeArea(.all)
+                VStack {
+                    Image("plato1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .ignoresSafeArea(.all)
+                    
+                    DescriptionView()
+                        .offset(y: -50)
+                }
+                
+            }
+        }
     }
 }
 
 #Preview {
     BusinessView()
+}
+
+struct DescriptionView: View {
+    var body: some View {
+        VStack (alignment: .center) {
+            Text("Restaurante 1")
+                .font(.title)
+                .fontWeight(.bold)
+            HStack (spacing: 4) {
+                ForEach(0 ..< 5) { item in
+                    Image("star")
+                }
+                Text("(4.9)")
+                    .opacity(0.5)
+                    .padding(.leading, 8)
+                Spacer()
+            }
+        }
+        .padding()
+        .padding(.top)
+        .background(Color("Bg"))
+        .clipShape(RoundedRectangle(cornerRadius: 40))
+        
+        Text("Platos Recomendados")
+            .font(.custom("IstokWeb-Bold", size: 24))
+            .padding(.horizontal)
+            .padding(.top)
+        ScrollView (.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0 ..< 5) { item in
+                    CardDishView(name: "Plato \(item + 1)", description: "Descripcion de todos los platos para visualizarlo cuando se esta en el restaurante y el propio plato", image: Image("plato\(item+1)"), price: "30", size: 170)
+                }
+                .padding(.trailing)
+            }
+        }
+        .padding(.leading)
+        .padding(.trailing)
+    }
 }
